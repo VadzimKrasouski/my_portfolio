8d40d1cf360857fd80b60d5253b7f4e60b6432a5
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-04-03 21:31:38 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import Main from "./Main";
+
+const App = () => (
+    <div className="App">
+        <div className="portfolio">
+            <Header/>
+            <Main/>
+        </div>
+    </div>
+);
+
+export default App;
