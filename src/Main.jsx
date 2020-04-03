5bf44f712d6fb89f5e4ca6ef969c88f05cac834e
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-04-03 23:23:56 +0200

    new futures in header and main

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,7 +3,16 @@ import styles from './Main.module.css'
 
 const Main = () => (
     <div className={styles.main}>
-
+        <div className={styles.container}>
+            <div className={styles.greating}>
+                <span>Привет!</span>
+                <span>Меня зовут Вадим Красовский</span>
+                <span>Я Front-end разработчик.</span>
+            </div>
+            <div className={styles.photo}>
+                <img src="#link" alt=""/>
+            </div>
+        </div>
     </div>
 );
 
---
+++ src/NavMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './NavMenu.module.css'
+
+const NavMenu = () => (
+    <div className={styles.navMenu}>
+        <a href="" className={styles.link}>Главная</a>
+        <a href="" className={styles.link}>Скиллы</a>
+        <a href="" className={styles.link}>Проекты</a>
+        <a href="" className={styles.link}>Контакты</a>
+    </div>
+);
+
+export default NavMenu;
