a1adb4f0e78347d1287a04460be37db855a9f5fe
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-04-22 12:46:09 +0200

    Updates

---
+++ precache-manifest.283c26ee3c84609fe0e3abe25fda109a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac86bb9dfe5cf8520770bc285a31063c",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "85c568f3761fc49b2a8f",
+    "url": "/my_portfolio/static/css/main.e77abb54.chunk.css"
+  },
+  {
+    "revision": "557d9ed0ba75c42d820a",
+    "url": "/my_portfolio/static/js/2.eac6db74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_portfolio/static/js/2.eac6db74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c568f3761fc49b2a8f",
+    "url": "/my_portfolio/static/js/main.c01bb529.chunk.js"
+  },
+  {
+    "revision": "907a72b385a7f7b89dad",
+    "url": "/my_portfolio/static/js/runtime-main.73ea3871.js"
+  }
+]);
