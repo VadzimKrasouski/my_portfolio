b3afb758df06f47f6db271862811c45e5406d229
VadzimKrasouski <vadzim.krasouski@gmail.com>
2020-06-09 15:24:23 +0200

    -refactored contacts

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -6,13 +6,56 @@ const Contact = () => (
     <div className={styles.contact}>
         <div className={styles.container}>
             <Title text={'Contact Me'}/>
-            <form className={styles.contactForm}>
-                <input type="text" placeholder="Name"/>
-                <input type="email" placeholder="E-mail"/>
-                <textarea rows="7" placeholder="Your message"/>
-                <button className={`${styles.btn} ${styles.radius_btn}`}
-                        type="submit">Send message</button>
-            </form>
+            <div className={styles.contactBlock}>
+                <div className={styles.contLeft}>
+                    <div className={styles.media}>
+                        <div className={styles.mediaIcon}>Icon</div>
+                        <div className={styles.mediaBody}>
+                            <h4>Email</h4>
+                            <span>vadzim.krasouski@gmail.com</span>
+                        </div>
+                    </div>
+                    <div className={styles.media}>
+                        <div className={styles.mediaIcon}>Icon</div>
+                        <div className={styles.mediaBody}>
+                            <h4>Phone</h4>
+                            <span>+48 576 088 155</span>
+                        </div>
+                    </div>
+                    <div className={styles.media}>
+                        <div className={styles.mediaIcon}>Icon</div>
+                        <div className={styles.mediaBody}>
+                            <h4>Skype</h4>
+                            <span>vadik_hj</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.contRight}>
+                    <form className={styles.contactForm}>
+                        <div className={styles.formName}>
+                            <input type="text" placeholder="Enter your name"
+                                   className={styles.formControl}/>
+                        </div>
+                        <div className={styles.formEmail}>
+                            <input type="email" placeholder="Your email address"
+                                   className={styles.formControl}/>
+                        </div>
+                        <div className={styles.formTitle}>
+                            <input type="text" placeholder="Enter the discussion title"
+                                   className={styles.formControl}/>
+                        </div>
+                        <div className={styles.formTextArea}>
+                            <textarea placeholder='Write your message'
+                                      className={styles.formControl}/>
+                        </div>
+                        <div className={styles.formButton}>
+                            <button className={`${styles.btn} ${styles.radius_btn}`}
+                                    type="submit">Send message now
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 );
